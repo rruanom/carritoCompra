ecdedcba630f2500386a677edf179471d6d79397
EduFatou <edufatou@gmail.com>
2024-05-27 15:40:23 +0200

    started in develop branch

---
+++ main.js
@@ -0,0 +1,28 @@
+const productos = [{
+    'alimento': 'tomate',
+    'cantidad': 0
+},
+{
+    'alimento': 'lechuga',
+    'cantidad': 0
+},
+{
+    'alimento': 'pollo',
+    'cantidad': 0
+},
+{
+    'alimento': 'queso',
+    'cantidad': 0
+},
+{
+    'alimento': 'agua',
+    'cantidad': 0
+},
+{
+    'alimento': 'bollo',
+    'cantidad': 0
+},
+{
+    'alimento': 'pavo',
+    'cantidad': 0
+}]
